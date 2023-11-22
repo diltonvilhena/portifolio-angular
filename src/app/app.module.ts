c3e13d6b42845efa3b3c0b6f2a2e265debfca173
19/05 1.1 <diltonvilhena@gmail.com>
2023-11-22 15:57:29 -0300

    feat: contrução sessão main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainComponent } from './components/main/main.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
