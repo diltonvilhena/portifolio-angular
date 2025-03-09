93305daeceb8cb11d628a892722b68d2b76db385
19/05 1.1 <diltonvilhena@gmail.com>
2025-03-09 17:06:49 -0300

    feat: atualizações skills

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface Skill {
+  caminho: string;
+  alt: string;
+  titulo: string;
+}
+
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
@@ -7,14 +13,10 @@ import { Component } from '@angular/core';
 })
 export class SkillsComponent {
 
-  skills: {caminho:string , alt:string, titulo:string}[] = [
-    {caminho:'/assets/icons/html.png',alt: 'ícone HTML',titulo:'HTML'},
-    {caminho:'/assets/icons/css.png',alt: 'ícone CSS',titulo:'CSS'},
-    {caminho:'/assets/icons/angular.png',alt: 'ícone Angular',titulo:'Angular'},
-    {caminho:'/assets/icons/java.png',alt: 'ícone Java',titulo:'JAVA'},
-    {caminho:'/assets/icons/flutter.png',alt: 'ícone Flutter',titulo:'Flutter'},
-    {caminho:'/assets/icons/wordpress.png',alt: 'ícone Wordpress',titulo:'Wordpress'},
-    {caminho:'/assets/icons/joomla.png',alt: 'ícone Joomla',titulo:'Joomla'},
-  ]
+  skills: Skill[] = [
+    { caminho: '/assets/icons/java.png', alt: 'ícone Java', titulo: 'Java' },
+    { caminho: '/assets/icons/angular.png', alt: 'ícone Angular', titulo: 'Angular' },
+    { caminho: '/assets/icons/flutter.png', alt: 'ícone Flutter', titulo: 'Flutter' },
+  ];
 
 }
