21b0766055a50bb1e7bcc2d497043ea94d11ba1d
19/05 1.1 <diltonvilhena@gmail.com>
2025-03-13 20:54:53 -0300

    feat: adição componente contact

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class ContactComponent {
 
+  contatos = [
+    {
+      tipo: 'Whatsapp',
+      icon: 'whatsapp.png',
+      link: 'https://wa.me/+5591980465126',
+      numero: '+55 91 98046-5126'
+    },
+    {
+      tipo: 'Email',
+      icon: 'email.png',
+      link: 'mailto:diltonvilhena@gmail.com',
+      numero: 'diltonvilhena@gmail.com'
+    }
+  ];
+
+  redes = [
+    {
+      nome: 'Linkedin',
+      link: 'https://www.linkedin.com/in/diltonvilhena/',
+      icon: 'linkedin-rodape.png'
+    },
+    {
+      nome: 'Github',
+      link: 'https://github.com/DiltonVilhena',
+      icon: 'github-rodape.png'
+    }
+  ];
+
 }
