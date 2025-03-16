923c2fb67ca133f4cd8ef065127a1b041dea5849
19/05 1.1 <diltonvilhena@gmail.com>
2025-03-15 22:38:28 -0300

    feat: adição novas habilidades

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -15,8 +15,14 @@ export class SkillsComponent {
 
   skills: Skill[] = [
     { caminho: '/assets/icons/java.png', alt: 'ícone Java', titulo: 'Java' },
+    { caminho: '/assets/icons/spring-boot.png', alt: 'ícone Spring Boot', titulo: 'Spring Boot' },
+    { caminho: '/assets/icons/typescript.png', alt: 'ícone typescript', titulo: 'Typescript' },
     { caminho: '/assets/icons/angular.png', alt: 'ícone Angular', titulo: 'Angular' },
+    { caminho: '/assets/icons/sql.png', alt: 'ícone SQL', titulo: 'SQL' },
+    { caminho: '/assets/icons/git.png', alt: 'ícone GIT', titulo: 'GIT' },
+    { caminho: '/assets/icons/dart.png', alt: 'ícone Dart', titulo: 'Dart' },
     { caminho: '/assets/icons/flutter.png', alt: 'ícone Flutter', titulo: 'Flutter' },
+    { caminho: '/assets/icons/wordpress.png', alt: 'ícone WordPress', titulo: 'WordPress' },
   ];
 
 }
